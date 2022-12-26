aef657296c0f04303a5a0219ade53b50f5e7a3ab
bchyes <bchyes@126.com>
2022-12-26 16:20:55 +0800

    initial_commit

--- src/cpu.v
+++ src/cpu.v
@@ -9,6 +9,7 @@
 `include "register.v"
 `include "rs.v"
 `include "slb.v"
+`include "rob.v"
 module cpu(
   input  wire                 clk_in,			// system clock signal
   input  wire                 rst_in,			// reset signal
